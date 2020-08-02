564a00e9f0fd368407b4374ecae7381a4fba1613
Tirth Shroff <54340408+tirth2801@users.noreply.github.com>
2020-08-02 22:50:10 +0530

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        Note Keeper...
+        <span role="img" aria-label="emoji">
+          🧠
+        </span>
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
